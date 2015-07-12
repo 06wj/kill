d3492fe1fe3e2213694d086e61aaa8f22526c78c
墨水 <wangjin.wj@alibaba-inc.com>
2015-07-12 15:59:46 +0800

    update states

--- src/js/states.js
+++ src/js/states.js
@@ -3,51 +3,10 @@ KISSY.add("kill/states", function(){
         //----------------------- stage 1 ----------------------------
         {
             bangs:[{
-                    x:200,
-                    y:300,
-                    length:200,
-                    rotation:0
-                },{
-                    x:400,
-                    y:0,
-                    rotation:90,
-                    length:300
-                },{
-                    x:300,
-                    y:400,
-                    length:200,
-                    rotation:0
-                },{
-                    x:600,
-                    y:70,
-                    rotation:90,
-                    length:300
-                }
-            ],
-            monsters:[{
-                    x: 250,
-                    y: 100,
-                    type:"boss"
-                }
-            ],
-            players:[{
-                    x:50,
-                    y:300,
-                    playerNum:0
-                },{
-                    x:750,
-                    y:300,
-                    playerNum:1
-                }
-            ]
-        },
-        //----------------------- stage 2 ----------------------------
-        {
-            bangs:[{
-                    x:400,
+                   x:400,
                     y:300,
                     length:300,
-                    rotation:0
+                    rotation:0,
                 },{
                     x:700,
                     y:100,
@@ -66,13 +25,75 @@ KISSY.add("kill/states", function(){
                 }
             ],
             monsters:[{
-                    x: 500,
-                    y: 100,
-                    type:"boss"
+                    x: 450,
+                    y: 250,
+                    type:"boss",
+                    isMove:false
+                },
+                {
+                    x: 100,
+                    y: 500,
+                    type:"normal",
+                    isMove:false
+                },
+                {
+                    x: 850,
+                    y: 500,
+                    type:"trap",
+                    isMove:false
+                }
+            ],
+            players:[{
+                    x:450,
+                    y:0,
+                    playerNum:0
                 },{
-                    x: 500,
+                    x:950,
+                    y:300,
+                    playerNum:1
+                }
+            ]
+        },
+        //----------------------- stage 2 ----------------------------
+        {
+            bangs:[{
+                   x:400,
+                    y:350,
+                    length:200,
+                    rotation:0
+                },{
+                    x:500,
+                    y:150,
+                    rotation:90,
+                    length:300
+                },{
+                    x:500,
+                    y:500,
+                    length:200,
+                    rotation:0
+                },{
+                    x:80,
+                    y:100,
+                    rotation:90,
+                    length:300
+                }
+            ],
+            monsters:[{
+                    x: 550,
                     y: 300,
-                    type:"boss"
+                    type:"normal"
+                },{
+                    x: 550,
+                    y: 400,
+                    type:"trap"
+                },{
+                    x: 400,
+                    y: 300,
+                    type:"normal"
+                },{
+                    x: 400,
+                    y: 400,
+                    type:"trap"
                 }
             ],
             players:[{
@@ -80,13 +101,232 @@ KISSY.add("kill/states", function(){
                     y:300,
                     playerNum:0
                 },{
-                    x:750,
+                    x:600,
+                    y:550,
+                    playerNum:1
+                }
+            ]
+        },
+        //----------------------- stage 3 ----------------------------
+        {
+            bangs:[{
+                    x:400,
+                    y:350,
+                    length:200,
+                    rotation:0
+                },{
+                    x:500,
+                    y:400,
+                    rotation:90,
+                    length:300
+                },{
+                    x:500,
+                    y:100,
+                    length:200,
+                    rotation:0
+                },{
+                    x:900,
+                    y:300,
+                    rotation:90,
+                    length:300
+                }
+            ],
+            monsters:[{
+                    x: 550,
+                    y: 300,
+                    type:"normal"
+                },{
+                    x: 550,
+                    y: 400,
+                    type:"normal"
+                },{
+                    x: 400,
+                    y: 300,
+                    type:"normal"
+                },{
+                    x: 400,
+                    y: 400,
+                    type:"normal"
+                }
+            ],
+            players:[{
+                    x:600,
+                    y:50,
+                    playerNum:0
+                },{
+                    x:1000,
                     y:300,
                     playerNum:1
                 }
             ]
         },
-
+        //----------------------- stage 4 ----------------------------
+        {
+            bangs:[{
+                    x:400,
+                    y:300,
+                    length:200,
+                    rotation:0
+                },{
+                    x:350,
+                    y:200,
+                    rotation:90,
+                    length:300
+                },{
+                    x:400,
+                    y:500,
+                    length:200,
+                    rotation:0
+                },{
+                    x:650,
+                    y:200,
+                    rotation:90,
+                    length:300
+                }
+            ],
+            monsters:[{
+                    x: 50,
+                    y: 100,
+                    type:"normal"
+                },{
+                    x: 900,
+                    y: 100,
+                    type:"normal"
+                },{
+                    x: 900,
+                    y: 500,
+                    type:"normal"
+                },{
+                    x: 500,
+                    y: 50,
+                    type:"boss"
+                }
+            ],
+            players:[{
+                    x:400,
+                    y:400,
+                    playerNum:0
+                },{
+                    x:500,
+                    y:400,
+                    playerNum:1
+                }
+            ]
+        },
+        //----------------------- stage 5 ----------------------------
+       {
+            bangs:[{
+                    x:400,
+                    y:300,
+                    length:200,
+                    rotation:0
+                },{
+                    x:350,
+                    y:200,
+                    rotation:90,
+                    length:300
+                },{
+                    x:400,
+                    y:500,
+                    length:200,
+                    rotation:0
+                },{
+                    x:650,
+                    y:200,
+                    rotation:90,
+                    length:300
+                }
+            ],
+            monsters:[{
+                    x: 50,
+                    y: 100,
+                    type:"trap"
+                },{
+                    x: 900,
+                    y: 100,
+                    type:"trap"
+                },{
+                    x: 500,
+                    y: 50,
+                    type:"boss"
+                }
+            ],
+            players:[{
+                    x:400,
+                    y:400,
+                    playerNum:0
+                },{
+                    x:500,
+                    y:400,
+                    playerNum:1
+                }
+            ]
+        },
+        //----------------------- stage 6 ----------------------------
+        {
+            bangs:[{
+                    x:400,
+                    y:300,
+                    length:200,
+                    rotation:0
+                },{
+                    x:350,
+                    y:200,
+                    rotation:90,
+                    length:300
+                },{
+                    x:400,
+                    y:500,
+                    length:200,
+                    rotation:0
+                },{
+                    x:650,
+                    y:200,
+                    rotation:90,
+                    length:300
+                }
+            ],
+            monsters:[{
+                    x: 150,
+                    y: 100,
+                    type:"normal"
+                },{
+                    x: 350,
+                    y: 100,
+                    type:"normal"
+                },{
+                    x: 450,
+                    y: 100,
+                    type:"normal"
+                },{
+                    x: 650,
+                    y: 100,
+                    type:"normal"
+                },{
+                    x: 900,
+                    y: 500,
+                    type:"normal"
+                },{
+                    x: 900,
+                    y: 500,
+                    type:"trap"
+                },{
+                    x: 500,
+                    y: 50,
+                    type:"boss"
+                }
+            ],
+            players:[{
+                    x:400,
+                    y:400,
+                    playerNum:0
+                },{
+                    x:500,
+                    y:400,
+                    playerNum:1
+                }
+            ]
+        }
     ];
 
     var states = {

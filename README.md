42afac44257d3ed292524d2555f57c73b6dfd49b
06wj <06wj@163.com>
2016-06-16 15:43:43 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# kill
+# kill 井杀
+
+### demo
+[./demo/index.html](http://06wj.github.io/kill/demo/index.html)
+
+### build
+
+* ```gulp```构建脚本
+* ```gulp watch```执行watch
--- src/entry.js
+++ src/entry.js
@@ -3,17 +3,17 @@ var config = require('./js/config');
 var game = require('./js/game');
 
 var gameContainer = document.getElementById("game");
-var startBg = document.getElementById("startBg");
+var startBgStyle = document.getElementById("startBg").style;
 if(config.noStart){
-    startBg.style.display = "none";
+    startBgStyle.display = "none";
     game.init(gameContainer);
 }
 else{
     document.body.onclick = function(){
-        startBg.style.background = "url(../build/img/infoBg.jpg)";
+        startBgStyle.background = "url(../build/img/infoBg.jpg)";
         document.body.onclick = function(){
             document.body.onclick = null;
-            startBg.style.display = "none";
+            startBgStyle.display = "none";
             game.init(gameContainer);
         }
     }
